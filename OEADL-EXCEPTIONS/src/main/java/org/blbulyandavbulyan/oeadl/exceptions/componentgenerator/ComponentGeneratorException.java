1e22e3324f58fbca4b61b90506ae34e3dcef7c04
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-04-15 15:56:28 +0400

    Removed module OEADL-EXCEPTIONS

--- OEADL-EXCEPTIONS/src/main/java/org/blbulyandavbulyan/oeadl/exceptions/componentgenerator/ComponentGeneratorException.java
+++
@@ -1,9 +0,0 @@
-package org.blbulyandavbulyan.oeadl.exceptions.componentgenerator;
-
-import org.blbulyandavbulyan.oeadl.exceptions.OEADLException;
-
-public abstract class ComponentGeneratorException extends OEADLException {
-    public ComponentGeneratorException(String msg){
-        super(msg);
-    }
-}
--- OEADL-EXCEPTIONS/src/main/java/org/blbulyandavbulyan/oeadl/exceptions/invalidtype/InvalidTypeException.java
+++
@@ -1,9 +0,0 @@
-package org.blbulyandavbulyan.oeadl.exceptions.invalidtype;
-
-import org.blbulyandavbulyan.oeadl.exceptions.OEADLException;
-
-public abstract class InvalidTypeException extends OEADLException {
-    public InvalidTypeException(String msg) {
-        super(msg);
-    }
-}
